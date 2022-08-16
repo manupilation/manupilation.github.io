f29823c6d414b3c8ca49303b9eadaae8c38036d1
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-08-16 01:36:53 -0300

    feat: add noBuild page

---
+++ personal-page-app/src/pages/Build.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NoBuildBody from "../components/NoBuilded/NoBuildBody";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+function BuildPage() {
+  return (
+    <>
+      {/* <Header /> */}
+      <NoBuildBody />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default BuildPage;
