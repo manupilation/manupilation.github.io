f607e212fa65179eb6d1e3dbe5aec1cad1cdf0aa
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-08-16 01:36:43 -0300

    feat: add home page

---
+++ personal-page-app/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import MainContent from "../components/Main-content/Main-content";
+import Footer from "../components/Footer/Footer";
+
+function HomePage() {
+  return(
+    <>
+      {/* <Header /> */}
+      <MainContent />
+      {/* <Footer /> */}
+    </>
+  )
+}
+
+export default HomePage;
+
