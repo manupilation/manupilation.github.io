7eb99acdfeb48d42e4d3b0a6302edb15e488b236
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-08-16 01:36:20 -0300

    refactor: update presentation and header components

--- personal-page-app/src/components/Presentation-section/Presentation-section.tsx
+++ personal-page-app/src/components/Presentation-section/Presentation-section.tsx
@@ -6,9 +6,10 @@ function PresentationSection() {
     <section className={styles.presentationSection}>
       <article>
         <h2>Olá! Boas-vindas ao meu portfólio.</h2>
-        <p>Conhecido pelos amigos como "Manu", acredito na tecnologia como ponte para igualdade entre todos. Atuo com tecnologias que podem ser verificadas <a href="https://github.com/manupilation" target="_blank"
+        <p>Chamado pelos amigos e colegas de "Manu", acredito na tecnologia como ponte para igualdade entre todos. Atuo com tecnologias que podem ser verificadas <a href="https://github.com/manupilation" target="_blank"
         rel="external">Aqui</a>.</p>
         <a className={styles.projects} href="https://github.com/manupilation?tab=repositories"
+        target="_blank"
         rel="external">Projetos</a>
       </article>
       <div className={styles.selfImage}>
