437f747730a2f5ed6c37e8395492be77324ee357
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-11-21 20:33:39 -0300

    feat: renderNavBar

--- personal-page-app/src/components/NavBar/Navbar.tsx
+++ personal-page-app/src/components/NavBar/Navbar.tsx
@@ -6,16 +6,21 @@ function handleClick(event: React.MouseEvent<HTMLElement>) {
   event.currentTarget.nextElementSibling?.classList.toggle(styles['active']);
   event.currentTarget.classList.toggle(styles['active']);
 }
+function renderNavBar() {
   return (
-    <nav className={styles.navContainer}>
-      <ul className={styles.navList}>
-        <Link to="/exp">Experiência</Link>
-        <Link to="/formacao">Formação</Link>
-        <Link to="/projects">Projetos</Link>
-        <Link to="/contact">Contato</Link>
-      </ul>
-    </nav>
-  )
+    <div className={styles.navBar}>
+      <button className={styles.menu} onClick={ handleClick }>MENU</button>
+      <nav className={styles.navContainer}>
+        <ul className={styles.navList}>
+          <Link to="/exp">Experiência</Link>
+          <Link to="/formacao">Formação</Link>
+          <Link to="/projects">Projetos</Link>
+          <Link to="/contact">Contato</Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
 }
 
 export default Navbar;
