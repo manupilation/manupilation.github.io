44184e569fad431212686e28359c7d1ecd91ed59
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-11-22 01:29:42 -0300

    feat: outsideListener

--- personal-page-app/src/components/NavBar/Navbar.tsx
+++ personal-page-app/src/components/NavBar/Navbar.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
+const activeMenu = document.querySelectorAll('[data-menu="true"]');
 
 function handleClick(event: React.MouseEvent<HTMLElement>) {
   event.currentTarget.nextElementSibling?.classList.toggle(styles['active']);
   event.currentTarget.classList.toggle(styles['active']);
+}
+
+function useOutsideListener(ref: React.MutableRefObject<null | HTMLDivElement>) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent | TouchEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        activeMenu.forEach((itens) => {
+          itens.classList.remove(styles['active']);
+        });
+      };
+    };
+    document.addEventListener("touchstart", handleClickOutside);
+    return () => {
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [ref]);
 }
 
 function renderNavBar() {
