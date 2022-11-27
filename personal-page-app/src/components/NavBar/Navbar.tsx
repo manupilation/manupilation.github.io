92b3b756bf98b06addcf3eab002f1b08ac88b795
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-11-26 23:19:07 -0300

    feat: clickOutside

--- personal-page-app/src/components/NavBar/Navbar.tsx
+++ personal-page-app/src/components/NavBar/Navbar.tsx
@@ -1,35 +1,48 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState, useCallback, RefObject } from 'react';
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
-const activeMenu = document.querySelectorAll('[data-menu="true"]');
+const activeMenu = document.querySelector('[data-menu]');
 
-function handleClick(event: React.MouseEvent<HTMLElement>) {
-  event.currentTarget.nextElementSibling?.classList.toggle(styles['active']);
-  event.currentTarget.classList.toggle(styles['active']);
-}
 
-function useOutsideListener(ref: React.MutableRefObject<null | HTMLDivElement>) {
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent | TouchEvent) {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        activeMenu.forEach((itens) => {
-          itens.classList.remove(styles['active']);
-        });
-      };
-    };
-    document.addEventListener("touchstart", handleClickOutside);
-    return () => {
-      document.removeEventListener("touchstart", handleClickOutside);
-    };
-  }, [ref]);
+const useOutsideListener = (): [
+  RefObject<HTMLDivElement>,
+  (node: HTMLDivElement) => void
+] => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const setRef = useCallback((node: HTMLDivElement) => {
+    ref.current = node;
+  }, [ref.current]);
+
+  return [ref, setRef]
 }
 
 function renderNavBar() {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  useOutsideListener(wrapperRef);
+  const [isActive, setIsActive] = useState(false)
+  const [ref, setRef] = useOutsideListener();
+
+  function handleClick(event: React.MouseEvent<HTMLElement>) {
+    event.currentTarget.nextElementSibling?.classList.toggle(styles['active']);
+    setIsActive(!isActive)
+  }
+
+  useEffect(() => {
+    if(ref.current?.attributes[1].value === 'true') {
+      
+      document.body.addEventListener('touchstart', (event) => {
+        setTimeout(() => {
+          const menuButton = (event.target as HTMLElement).parentElement;
+          if(menuButton !== ref.current) {
+            ref.current?.querySelector('nav')?.classList.remove(styles['active']);
+            document.body.removeEventListener('touchstart', () => {});
+        }}, 75);
+      });
+    };
+    
+  }, [ref.current?.attributes[1].value]);
 
   return (
-    <div className={styles.navBar} ref={wrapperRef}>
+    <div className={styles.navBar} ref={ref} data-isactive={ isActive.toString() }>
       <button className={styles.menu} onClick={ handleClick } data-menu>MENU</button>
       <nav className={styles.navContainer} data-menu>
         <ul className={styles.navList}>
