aac5621ee3705173309abd81f56dda833b620071
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-05 19:12:01 -0300

    feat: vitest config

---
+++ personal-page-app/tests/vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vitest/config';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
+})
