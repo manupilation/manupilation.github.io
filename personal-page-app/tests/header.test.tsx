df7849835029314cd21ee0aef49c9ced3c029128
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-07 00:27:09 -0300

    feat: click menu mobile test

--- personal-page-app/tests/header.test.tsx
+++ personal-page-app/tests/header.test.tsx
@@ -2,8 +2,8 @@
  * @vitest-environment jsdom
  */
  import React from 'react';
- import { describe, test, expect, beforeAll, afterAll } from 'vitest';
- import { cleanup, render, screen } from '@testing-library/react';
+ import { describe, test, expect, beforeAll, afterAll, afterEach } from 'vitest';
+ import { cleanup, fireEvent, getByRole, render, screen } from '@testing-library/react';
  import Header from '../src/components/Header/Header';
  import { BrowserRouter } from 'react-router-dom';
 
@@ -40,4 +40,28 @@ describe('Testa o componente header', () => {
 
     expect(getLinks).toHaveLength(4)
   });
+});
+
+describe('Testes relacionados as funcionalidades e eventos do Header', () => {
+  let renderHeader;
+  beforeAll(() => {
+    renderHeader = render(<Header />, {
+      wrapper: BrowserRouter
+    });
+  });
+
+  afterEach(cleanup);
+
+  test('Testa se o click na navegação mobile ativa a lista', () => {
+    document.body.style.width = '600px';
+
+    const getNavBar = screen.getByRole('navigation');
+    const getHeaderNav = screen.getByRole('button', {
+      name: /menu/i
+    });
+    fireEvent.click(getHeaderNav);
+    console.log(getNavBar.classList[1]);
+
+    expect(getNavBar.className.includes('_active_')).toBe(true);
+  })
 });
