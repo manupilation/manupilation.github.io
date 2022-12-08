6b764d262d330a805a2eb76014943cc2b5d137f9
Emanoel Mattos <emanoelmattos13@gmail.com>
2022-12-07 23:21:12 -0300

    refactor: maincontent import

--- personal-page-app/tests/mainContent.test.tsx
+++ personal-page-app/tests/mainContent.test.tsx
@@ -4,6 +4,6 @@
  import React from 'react';
  import { describe, test, expect, beforeAll, afterAll } from 'vitest';
  import { cleanup, fireEvent, render, screen } from '@testing-library/react';
- import Header from '../src/components/Header/Header';
+ import MainContent from '../src/components/Main-content/Main-content';
  import { BrowserRouter } from 'react-router-dom';
 
