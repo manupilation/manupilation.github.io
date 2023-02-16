73bb537398c436b8f0115f9a149403982272fc06
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-02-16 16:03:14 -0300

    feat: Project page

---
+++ personal-page-app/src/pages/Projects.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Projects from "../components/Projects/Projects";
+import Footer from "../components/Footer/Footer";
+
+function ProjectsPage() {
+  return(
+    <>
+      <Projects />
+    </>
+  )
+}
+
+export default ProjectsPage;
+
