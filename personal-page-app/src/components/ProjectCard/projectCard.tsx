6b30e0c928bce6bcc54a10e60fe0a466dcaa801c
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-02-19 22:34:34 -0300

    feat: projectsCard

--- personal-page-app/src/components/ProjectCard/projectCard.tsx
+++ personal-page-app/src/components/ProjectCard/projectCard.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Project } from "../Projects/Projects";
 import githubIcon from "../../../images/githubButton.svg";
+import style from "./projectCard.module.scss";
 
 
 function ProjectCard(props: { project: Project }) {
   const { project } = props;
   return(
-    <section>
-      <div>
+    <section className={style.cardSection}>
+      <div className={style.cardContainer}>
         <h2>{project.title}</h2>
         <p>{project.details}</p>
 
-        <div>
+        <div className={style.containerLinks}>
           <div>
-            <button><img src={ githubIcon } alt="Github icon"/></button>
+            <a href={project.github} target="_blank"><img src={ githubIcon } alt="Github icon"/></a>
           </div>
           <h4>SAIBA MAIS</h4>
         </div>
