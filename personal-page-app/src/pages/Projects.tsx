507c3adf4b164fbb5b7fb1cf06775262c6e38e31
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-10 16:12:07 -0300

    refactor: ProjectsPage using SlideContext

--- personal-page-app/src/pages/Projects.tsx
+++ personal-page-app/src/pages/Projects.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Projects from "../components/Projects/Projects";
 import Footer from "../components/Footer/Footer";
+import SlideContext from "../context/slideContext";
 
 function ProjectsPage() {
   return(
     <>
-      <Projects />
+      <SlideContext >
+        <Projects />
+      </SlideContext>
     </>
   )
 }
