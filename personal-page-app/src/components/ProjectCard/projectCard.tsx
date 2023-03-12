f5de9f1f73e1182ea2211959e0477b26a643dec9
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-12 16:37:45 -0300

    refactor: projectCard structure

--- personal-page-app/src/components/ProjectCard/projectCard.tsx
+++ personal-page-app/src/components/ProjectCard/projectCard.tsx
@@ -6,9 +6,21 @@ import style from "./projectCard.module.scss";
 
 function ProjectCard(props: { project: Project }) {
   const { project } = props;
+
   return(
     <section className={style.cardSection}>
       <div className={style.cardContainer}>
+        <span> 
+          {
+            project.isDeployed
+            ? <a
+              className={style.deployed} 
+              target="_blank" 
+              href={project.deploy}
+              ></a>
+            : <div className={style.notDeployed}></div>
+          }
+        </span>
         <h2>{project.title}</h2>
         <p>{project.details}</p>
 
@@ -16,7 +28,7 @@ function ProjectCard(props: { project: Project }) {
           <div>
             <a href={project.github} target="_blank"><img src={ githubIcon } alt="Github icon"/></a>
           </div>
-          <h4>SAIBA MAIS</h4>
+          <h4>STACK</h4>
         </div>
       </div>
     </section>
