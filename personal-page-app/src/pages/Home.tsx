962c57fa91cbe260cccd8ba932c0153299ac2aa5
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-03-16 16:53:01 -0300

    refactor: Home with head

--- personal-page-app/src/pages/Home.tsx
+++ personal-page-app/src/pages/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../components/Header/Header";
 import MainContent from "../components/Main-content/Main-content";
 import Footer from "../components/Footer/Footer";
+import Head from "../Head";
 
 function HomePage() {
   return(
     <>
       {/* <Header /> */}
+      <Head title="Boas vindas!"/>
       <MainContent />
       {/* <Footer /> */}
     </>
