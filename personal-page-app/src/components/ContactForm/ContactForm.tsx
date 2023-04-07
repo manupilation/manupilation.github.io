12ce66a97df43954079e2fc8b772cc31dfdcac8a
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-07 17:00:48 -0300

    feat: Image component ContactForm

--- personal-page-app/src/components/ContactForm/ContactForm.tsx
+++ personal-page-app/src/components/ContactForm/ContactForm.tsx
@@ -4,14 +4,13 @@ import ManuImage from "../../../images/Manur.jpg";
 import {ReactComponent as Download} from '../../../images/download.svg';
 import ContactFormInputs from './ContactFormInputs';
 import AsideContact from './AsideContact';
+import Image from '../Helper/Image';
 
 const ContactForm = () => {
   return (
     <section className={styles.contactContainer  + " animaRight"}>
       <article className={styles.presentationContainer}>
-        <div className={styles.imageContainer}>
-          <img src={ManuImage} alt="Imagem do Manu. Atrás dele, uma cerca viva."/>
-        </div>
+        <Image src={ManuImage} alt="Imagem do Manu. Atrás dele, uma cerca viva." className={styles.imageContainer} />
         <a className={styles.downloadCV} href='/CurriculoManu.pdf' download="CV-Manu.pdf">
           <Download />
           <p>CV</p>
