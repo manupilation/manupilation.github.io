101acf76f7b07de992cebf933ee5456cbb5e1810
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-04-07 17:00:39 -0300

    feat: Image component className

--- personal-page-app/src/components/Helper/Image.tsx
+++ personal-page-app/src/components/Helper/Image.tsx
@@ -4,10 +4,11 @@ import styles from './Image.module.scss';
 type ImageProps = {
   src: string,
   alt: string,
+  className?: string,
 };
 
 const Image = (props: ImageProps) => {
-  const { src, alt } = props;
+  const { src, alt, className } = props;
   const [skeleton, setSkeleton] = useState(true);
 
   function handleOnLoad(event: SyntheticEvent<HTMLImageElement>) {
@@ -17,7 +18,7 @@ const Image = (props: ImageProps) => {
   }
 
   return (
-    <div className={styles.wrapper}>
+    <div className={`${styles.wrapper} ${className}`}>
       {skeleton && <div className={styles.skeleton}></div>}
       <img onLoad={handleOnLoad} className={styles.img} alt={alt} src={src}/>
     </div>
