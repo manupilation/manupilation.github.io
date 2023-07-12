ea085ddacc93cbe2f70a83c4f9022ade63774528
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-12 17:11:22 -0300

    refactor-test: move test files

--- personal-page-app/tests/Components/header.test.tsx
+++ personal-page-app/tests/Components/header.test.tsx
@@ -4,7 +4,7 @@
  import React from 'react';
  import { describe, test, expect, beforeAll, afterAll } from 'vitest';
  import { cleanup, fireEvent, render, screen } from '@testing-library/react';
- import Header from '../src/components/Header/Header';
+ import Header from '../../src/components/Header/Header';
  import { BrowserRouter } from 'react-router-dom';
 
 function responsiveScreen() {
--- personal-page-app/tests/Components/mainContent.test.tsx
+++ personal-page-app/tests/Components/mainContent.test.tsx
@@ -4,7 +4,7 @@
  import React from 'react';
  import { describe, test, expect, beforeAll, afterAll } from 'vitest';
  import { cleanup, render, screen } from '@testing-library/react';
- import MainContent from '../src/components/Main-content/Main-content';
+ import MainContent from '../../src/components/Main-content/Main-content';
  import { BrowserRouter } from 'react-router-dom';
 
 
