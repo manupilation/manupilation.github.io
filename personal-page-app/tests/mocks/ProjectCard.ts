b43c8f254c3958936af16e9957d9851fd1c34b92
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-12 18:19:16 -0300

    test-feat: ProjectCardMock

---
+++ personal-page-app/tests/mocks/ProjectCard.ts
@@ -0,0 +1,10 @@
+const ProjectCardMock = {
+  title: "Mangustus",
+  details: "Lorem ipsum",
+  isDeployed: true,
+  deploy: "url",
+  github: "url",
+  seeMore: "abcde",
+}
+
+export default { ProjectCard: ProjectCardMock };
