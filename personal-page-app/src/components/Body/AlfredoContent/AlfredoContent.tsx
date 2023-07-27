7c18e380d7758d7d84a636efd94ecc7ca166141e
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-27 15:01:17 -0300

    feat: Alfredo Content

---
+++ personal-page-app/src/components/Body/AlfredoContent/AlfredoContent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from '../../Helper/Image';
+import Separator from '../../Helper/Separator';
+
+const AlfredoContent = () => {
+  return (
+    <main>
+      <Image src='' alt=''/>
+
+      <Separator />
+
+      <article>
+        <h2>Conheça o Alfredo!</h2>
+
+        <p>
+          Alfredo é o meu pet. Alfredo é um gatinho castrado super curioso e extremamente sociável. Ele ama crianças, sair para passeios, sachês e petiscos.
+          Milhares de animais estão neste momento pelas ruas precisando de um lar. Você pode fazer a diferença! Doe para uma ONG ou adote um Pet ❤️
+        </p>
+      </article>
+    </main>
+  );
+}
+
+export default AlfredoContent;
