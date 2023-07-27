395388beec3ab546e433c89afddb1051ea241173
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-27 14:44:37 -0300

    feat: AbordagemSection

---
+++ personal-page-app/src/components/Body/SecondBody/AbordagemSection/AbordagemSection.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AbordagemSection = () => {
+  return (
+    <section>
+      <h2>Minha abordagem consiste em...</h2>
+      <p>
+        Analisar o problema e pensar em caminhos para resolvê-lo. Acredito que o trabalho em equipe é a chave para resultados mais expressivos.
+      </p>
+    </section>
+  )
+}
+
+export default AbordagemSection;
