b96bdc8ca951084b0f5bf56d464ad00b2cb74d33
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-28 15:49:30 -0300

    feat: AbordagemSection classesNames

--- personal-page-app/src/components/Body/SecondBody/AbordagemSection/AbordagemSection.tsx
+++ personal-page-app/src/components/Body/SecondBody/AbordagemSection/AbordagemSection.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../SecondBody.module.scss';
 
 const AbordagemSection = () => {
   return (
-    <section>
+    <section className={styles.abordagemSection}>
       <h2>Minha abordagem consiste em...</h2>
       <p>
         Analisar o problema e pensar em caminhos para resolvê-lo. Acredito que o trabalho em equipe é a chave para resultados mais expressivos.
