2978637b3975c8872f169cabd9d7f7cebeaa3391
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-31 13:26:36 -0300

    feat: Alfredo Content  animation logic

--- personal-page-app/src/components/Body/AlfredoContent/AlfredoContent.tsx
+++ personal-page-app/src/components/Body/AlfredoContent/AlfredoContent.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from '../../Helper/Image';
 import Separator from '../../Helper/Separator';
 import Alfredo from '../../../../images/Alfredo.jpg';
 import styles from './AlfredoContent.module.scss';
+import UseRect from '../../../hooks/UseRect';
 
 const AlfredoContent = () => {
+  const { halfScreenHeight } = UseRect();
+  const animateRef = useRef<HTMLElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (animateRef.current) {
+      const { top, bottom } = animateRef.current.getBoundingClientRect();
+      setIsVisible(bottom > 0 && top < (halfScreenHeight * 1.4));
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [halfScreenHeight]);
   return (
-    <main className={styles.mainAlfredo}>
+    <main 
+      className={styles.mainAlfredo}
+      ref={animateRef}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transform: 'translate3d(0, 0, 0)'
+      }}
+    >
       <Image src={Alfredo} alt='Alfredo prestes a atacar as mãos de um humano.' className={styles.catImage}/>
 
       <Separator />
