e53ac51d62ff6e86644350589ff6a7d70efbac91
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-07-31 13:26:11 -0300

    feat: second body animation logic

--- personal-page-app/src/components/Body/SecondBody/SecondBody.tsx
+++ personal-page-app/src/components/Body/SecondBody/SecondBody.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Image from '../../Helper/Image';
 import styles from './SecondBody.module.scss';
 import AbordagemSection from './AbordagemSection/AbordagemSection';
 import ExpSection from './ExpSection/ExpSection';
 import PeopleJob from '../../../../images/PeopleJob.png';
+import UseRect from '../../../hooks/UseRect';
 
 const SecondBody = () => {
+  const { halfScreenHeight } = UseRect();
+  const animateRef = useRef<HTMLElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (animateRef.current) {
+      const { top, bottom } = animateRef.current.getBoundingClientRect();
+      setIsVisible(bottom > 0 && top < halfScreenHeight);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [halfScreenHeight]);
+
   return (
-    <main className={styles.mainSecondBody}>
+    <main 
+      className={styles.mainSecondBody} 
+      ref={animateRef}
+      style={{
+        opacity: isVisible ? 1 : 0,
+        transform: 'translate3d(0, 0, 0)'
+      }}
+    >
       <div className={styles.secondBodyWrapper}>
         <AbordagemSection />
 
