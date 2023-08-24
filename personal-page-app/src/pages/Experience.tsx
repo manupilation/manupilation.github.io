28d3f98bbe2464d4e4233f0bfa4fd782fc1a7855
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-24 15:41:48 -0300

    feat: Experience Page structure

---
+++ personal-page-app/src/pages/Experience.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExperienceFilter from "../components/ExperienceFilter/ExperienceFilter";
+
+const Experience = () => {
+  return (
+    <main>
+      <ExperienceFilter />
+    </main>
+  );
+}
+
+export default Experience;
