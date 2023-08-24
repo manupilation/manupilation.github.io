a3929ea5ea3adbed8fc7ec1b5b25bdf5048e7948
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-24 16:03:16 -0300

    feat: experienceContext

---
+++ personal-page-app/src/context/experienceContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, ReactNode, useState } from "react";
+
+type ExperienceContextTypes = {
+  isFront: boolean,
+  isBack: boolean,
+  setIsFront: React.Dispatch<React.SetStateAction<boolean>>,
+  setIsBack: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+type ExperienceProps = {
+  children: ReactNode,
+}
+
+export const experienceContext = createContext<ExperienceContextTypes>(null!);
+
+const ExperienceContext = (props: ExperienceProps) => {
+  const [isFront, setIsFront] = useState<boolean>(true);
+  const [isBack, setIsBack] = useState<boolean>(false);
+
+  const experienceValue = {
+    isFront,
+    isBack,
+    setIsFront,
+    setIsBack,
+  }
+
+  return (
+    <experienceContext.Provider value={experienceValue}>
+      {props.children}
+    </experienceContext.Provider>
+  )
+}
+
+export default ExperienceContext;
