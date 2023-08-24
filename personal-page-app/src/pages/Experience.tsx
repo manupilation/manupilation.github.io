8aa3de61c18851ac71a8d51c3288768aa7969f70
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-24 16:04:17 -0300

    refactor: EXPContext rendering EXPFilter

--- personal-page-app/src/pages/Experience.tsx
+++ personal-page-app/src/pages/Experience.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ExperienceFilter from "../components/ExperienceFilter/ExperienceFilter";
 import Head from "../Head";
+import ExperienceContext from "../context/experienceContext";
 
 const Experience = () => {
   return (
     <main>
       <Head title="Meus conhecimentos" description="Entre em contato comigo! Vamos conversar um pouco sobre tecnologia :)"/>
-      <ExperienceFilter />
+      <ExperienceContext>
+        <ExperienceFilter />
+      </ExperienceContext>
     </main>
   );
 }
