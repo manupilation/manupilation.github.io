81c2f21bd401de836057b476366bb5a6c9b38166
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-24 15:55:57 -0300

    refactor: Meta Head on Experience page

--- personal-page-app/src/pages/Experience.tsx
+++ personal-page-app/src/pages/Experience.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ExperienceFilter from "../components/ExperienceFilter/ExperienceFilter";
+import Head from "../Head";
 
 const Experience = () => {
   return (
     <main>
+      <Head title="Meus conhecimentos" description="Entre em contato comigo! Vamos conversar um pouco sobre tecnologia :)"/>
       <ExperienceFilter />
     </main>
   );
