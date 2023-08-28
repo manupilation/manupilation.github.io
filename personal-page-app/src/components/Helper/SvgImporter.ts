a5fb1d966473a62428011f930c78ca0ab3a4bdb5
Emanoel Mattos <emanoelmattos13@gmail.com>
2023-08-28 20:29:52 -0300

    feat: SVG importer

---
+++ personal-page-app/src/components/Helper/SvgImporter.ts
@@ -0,0 +1,59 @@
+import Linux from '../../../images/exps/Linux.svg';
+import Git from '../../../images/exps/Git.svg';
+import Docker from '../../../images/exps/Docker.svg';
+import Shell from '../../../images/exps/Shell.svg';
+import Javascript from '../../../images/exps/Javascript.svg';
+import Typescript from '../../../images/exps/Typescript.svg';
+import HTML5 from '../../../images/exps/HTML5.svg';
+import CSS3 from '../../../images/exps/CSS3.svg';
+import Sass from '../../../images/exps/Sass.svg';
+import React from '../../../images/exps/React.svg';
+import Redux from '../../../images/exps/Redux.svg';
+import Next from '../../../images/exps/Next.js.svg';
+import Jest from '../../../images/exps/Jest.svg';
+import Cypress from '../../../images/exps/Cypress.svg';
+import RTL from '../../../images/exps/RTL.svg';
+import Figma from '../../../images/exps/Figma.svg';
+import Nodejs from '../../../images/exps/Node.js.svg';
+import Express from '../../../images/exps/Express.svg';
+import Python from '../../../images/exps/Python.svg';
+import Java from '../../../images/exps/Java.svg';
+import SQL from '../../../images/exps/SQL.svg';
+import MySQL from '../../../images/exps/MySQL.svg';
+import MongoDB from '../../../images/exps/MongoDB.svg';
+import Sequelize from '../../../images/exps/Sequelize.svg';
+import Prisma from '../../../images/exps/Prisma.svg';
+import Mongoose from '../../../images/exps/Mongoose.svg';
+import Mocha from '../../../images/exps/Mocha.svg';
+import Chai from '../../../images/exps/Chai.svg';
+
+export default {
+  Linux,
+  Git,
+  Docker,
+  Shell,
+  Javascript,
+  Typescript,
+  HTML5,
+  CSS3,
+  Sass,
+  React,
+  Redux,
+  "Next.js": Next,
+  Jest,
+  Cypress,
+  RTL,
+  Figma,
+  "Node.js": Nodejs,
+  Express,
+  Python,
+  Java,
+  SQL,
+  MySQL,
+  MongoDB,
+  Sequelize,
+  Prisma,
+  Mongoose,
+  Mocha,
+  Chai
+};
